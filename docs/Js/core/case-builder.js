5843786ca6de439a0b9b49f36fb70038c5af0a73
Instituto Delyone de Medicina e Tecnologia <instituto@delyone.com>
2026-09-18 01:17:08 -0300

    fix(cases): normalize accented investigation identifiers

--- docs/Js/core/case-builder.js
+++ docs/Js/core/case-builder.js
@@ -276,7 +276,7 @@
       return String(value || "")
         .toLowerCase()
         .normalize("NFD")
-        .replace(/[\\u0300-\\u036f]/g, "")
+        .replace(/[\u0300-\u036f]/g, "")
         .replace(/[^a-z0-9]+/g, "_")
         .replace(/^_+|_+$/g, "");
     }
